2d579c6a04e23f4202d79f1a96e0aaef99506f51
Prachiti <ppbapat29@gmail.com>
2023-03-31 22:39:56 +0530

    commit

--- dentist/website/urls.py
+++ dentist/website/urls.py
@@ -4,4 +4,11 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('contact.html', views.contact, name="contact"),
+    path('about.html', views.about, name="about"),
+    path('booking.html', views.booking, name='booking'),
+    path('room.html', views.room, name='room'),
+    path('service.html', views.service, name='service'),
+    path('team.html', views.team, name='team'),
+    path('testimonial.html', views.testimonial, name='testimonial')
 ]
--- dentist/website/views.py
+++ dentist/website/views.py
@@ -2,3 +2,24 @@ from django.shortcuts import render
 
 def home(request):
 	return render(request, 'home.html', {})
+
+def contact(request):
+	return render(request, 'contact.html', {})
+
+def about(request):
+	return render(request, 'about.html', {})
+
+def booking(request):
+	return render(request, 'booking.html', {})
+
+def room(request):
+	return render(request, 'room.html', {})
+
+def service(request):
+	return render(request, 'service.html', {})
+
+def team(request):
+	return render(request, 'team.html', {})
+
+def testimonial(request):
+	return render(request, 'testimonial.html', {})
