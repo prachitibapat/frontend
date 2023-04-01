29aa4554e42de34bf0e4662a95ffa0f15f6858f1
Fancrafter317 <ruchirjadhav29@gmail.com>
2023-04-01 19:01:05 +0530

    Additions to the login page

--- dentist/website/views.py
+++ dentist/website/views.py
@@ -1,25 +1,55 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+
 
 def home(request):
-	return render(request, 'home.html', {})
+    return render(request, 'home.html', {})
+
 
 def contact(request):
-	return render(request, 'contact.html', {})
+    return render(request, 'contact.html', {})
+
 
 def about(request):
-	return render(request, 'about.html', {})
+    return render(request, 'about.html', {})
+
 
 def booking(request):
-	return render(request, 'booking.html', {})
+    return render(request, 'booking.html', {})
 
-def room(request):
-	return render(request, 'room.html', {})
+
+def signup(request):
+
+    if request.method == "POST":
+        username = request.POST['username']
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        email = request.POST['email']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
+
+        myuser = User.objects.create_user(username, email, pass1)
+        myuser.first_name = fname
+        myuser.last_name = lname
+
+        myuser.save()
+
+        messages.success(
+            request, "Your Account has been created succesfully")
+        return redirect('home')
+
+    return render(request, 'signup.html', {})
+
 
 def service(request):
-	return render(request, 'service.html', {})
+    return render(request, 'service.html', {})
+
 
 def team(request):
-	return render(request, 'team.html', {})
+    return render(request, 'team.html', {})
+
 
 def testimonial(request):
-	return render(request, 'testimonial.html', {})
+    return render(request, 'testimonial.html', {})
